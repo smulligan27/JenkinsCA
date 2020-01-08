1115cf8fe39d96667279087a9dbe2e3a7f42c1fb
smulligan27 <43577062+smulligan27@users.noreply.github.com>
2020-01-08 13:11:41 +0000

    Update Student.java

--- Student.java
+++ Student.java
@@ -7,7 +7,6 @@ public class Student {
 
     public AttendanceGrade getAttendanceGrade(int attendance) {
         if (attendance < 0 || attendance > 100) throw new IllegalArgumentException();
-        if (attendance == 0) return AttendanceGrade.ABSENT;
         if (attendance < 30) return AttendanceGrade.VERY_POOR;
         if (attendance < 70) return AttendanceGrade.AVERAGE;
         if (attendance < 90) return AttendanceGrade.GOOD;
