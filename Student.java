c1f7d19c277429deb511acb351a3dad284583fe3
smulligan27 <43577062+smulligan27@users.noreply.github.com>
2020-01-08 11:40:30 +0000

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,20 @@
+/**
+ * Created by meudecc on 15/02/2017.
+ */
+public class Student {
+
+    public enum AttendanceGrade {ABSENT, VERY_POOR, POOR, AVERAGE, GOOD, VERY_GOOD};
+
+    public AttendanceGrade getAttendanceGrade(int attendance) {
+        if (attendance < 0 || attendance > 100) throw new IllegalArgumentException();
+        if (attendance == 0) return AttendanceGrade.ABSENT;
+        if (attendance < 30) return AttendanceGrade.VERY_POOR;
+        if (attendance < 70) return AttendanceGrade.AVERAGE;
+        if (attendance < 90) return AttendanceGrade.GOOD;
+        if (attendance >= 90) return AttendanceGrade.VERY_GOOD;
+        return AttendanceGrade.ABSENT;
+
+    }
+}
+
+
